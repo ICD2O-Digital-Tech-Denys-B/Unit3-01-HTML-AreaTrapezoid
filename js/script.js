9467166a5337b4051ade533120fdb6e7b34c05bd
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-04-08 15:03:25 +0000

    Finished

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+    function calcArea() {
+        let baseA = parseFloat(document.getElementById("base-a").value);
+        let baseB = parseFloat(document.getElementById("base-b").value);
+        let height = parseFloat(document.getElementById("height").value);
+        let area = 0.5*(baseA+baseB)*height
+        document.getElementById("answer").innerHTML = "Answer:" + area +" cm^2";
+}
